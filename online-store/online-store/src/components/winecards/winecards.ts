9b84aef47bff0d8465d4ec262956f6f431fe030e
ephedrini1 <ephedrini@yandex.ru>
2022-12-04 21:37:27 +0300

    refactor: refactor winecards rendering

--- online-store/online-store/src/components/winecards/winecards.ts
+++ online-store/online-store/src/components/winecards/winecards.ts
@@ -1,54 +1,67 @@
 import './winecards.css';
 import { Wine } from '../../types/products'
 import { LocalStorage, localStorageKeys } from '../../utils/localstorage';
+import {goods} from '../../constats/goods'
+import Component from '../../utils/component';
 
 class WineCards {
-  render(data: Wine[]) {
+  wineContainer: Component; 
+  constructor() {
+    this.wineContainer = new Component(document.body, 'div', 'container');
+  }
+
+  render() {
+    let wines: Wine[] = [];
     if (localStorage.getItem('goods')) {
-      data = LocalStorage.getLocalStorage(localStorageKeys.goods) || data;
+      wines = LocalStorage.getLocalStorage(localStorageKeys.goods) || goods;
     };
     if (localStorage.getItem('sorted')) {
-      data = LocalStorage.getLocalStorage(localStorageKeys.sorted) || data;
+      wines = LocalStorage.getLocalStorage(localStorageKeys.sorted) || goods;
     };
-    const wines = data;
-    const fragment = document.createDocumentFragment();
-    const wineCardTemp = document.querySelector('#winecardsItemTemp') as HTMLTemplateElement;
+    
     wines.forEach((item : Wine, idx : number) => {
-      const wineCard = wineCardTemp.content.cloneNode(true) as HTMLElement;
-
-      (wineCard.querySelector('.winecards__face-photo') as HTMLElement)!.style.backgroundImage = `url(${item.image})`;
-      (wineCard.querySelector('.winecards__face-name'))!.textContent = item.name;
-      (wineCard.querySelector('.winecards__face-count'))!.textContent = `В наличии : ${item.count}`;
-      (wineCard.querySelector('.winecards__face-price'))!.textContent = `${item.price} BYN`;
-      (wineCard.querySelector('.winecards__back-name'))!.textContent = item.name;
-      (wineCard.querySelector('.winecards__back-madeBy'))!.textContent = `Производитель : ${item.madeBy}`;
-      (wineCard.querySelector('.winecards__back-country'))!.textContent = `Страна : ${item.country}`;
-      (wineCard.querySelector('.winecards__back-year'))!.textContent = `Год : ${item.year}`;
-      (wineCard.querySelector('.winecards__back-color'))!.textContent = `Цвет : ${item.color}`;
-      (wineCard.querySelector('.winecards__back-alcohol'))!.textContent = `Крепость : ${item.alcohol}%`;
-      (wineCard.querySelector('.winecards__back-vol'))!.textContent = `Объем : ${item.vol}л`;
-
+      const wineCard = new Component(this.wineContainer.node, 'div', 'winecards__item');
+      
+      if (wineCard) {
+        const wineCardFace = new Component(wineCard.node, 'div', 'winecards-face');
+        const wineCardFaceDetails = new Component(wineCardFace.node, 'div', 'winecards-face__details');
+        const wineCardFaceName = new Component(wineCardFaceDetails.node, 'h2', 'winecards-face__name', `${item.name}`);
+        const wineCardFacePhoto = new Component(wineCardFaceDetails.node, 'div', 'winecards-face__photo');
+        wineCardFacePhoto.node.style.backgroundImage = `url(${item.image})`;
+        const wineCardFaceCount = new Component(wineCardFaceDetails.node, 'h3', 'winecards-face__count', `В наличии : ${item.count}`);
+        const wineCardFacePrice = new Component(wineCardFaceDetails.node, 'h3', 'winecards-face__price',`${item.price} BYN`);
+        const wineCardBack = new Component(wineCard.node, 'div', 'winecards-back');
+        const wineCardBackDetails = new Component(wineCardBack.node, 'div', 'winecards-back__details');
+        const wineCardBackName = new Component(wineCardBackDetails.node, 'h2', 'winecards-back__name', `${item.name}`);
+        const wineCardBackMadeBy = new Component(wineCardBackDetails.node, 'p','winecards-back__madeBy',`Производитель : ${item.madeBy}`);
+        const wineCardBackCountry = new Component(wineCardBackDetails.node, 'p','winecards-back__country', `Страна : ${item.country}`);
+        const wineCardBackYear = new Component(wineCardBackDetails.node, 'p','winecards-back__year', `Год : ${item.year}`);
+        const wineCardBackColor = new Component(wineCardBackDetails.node, 'p','winecards-back__color', `Цвет : ${item.color}`);
+        const wineCardBackAlcohol = new Component(wineCardBackDetails.node, 'p','winecards-back__alcohol', `Крепость : ${item.alcohol}%`);
+        const wineCardBackVol = new Component(wineCardBackDetails.node, 'p','.winecards-back__vol', `Объем : ${item.vol}л`);
+      }
 
       if (!item.inStock || item.count === 0) {
-        (wineCard.querySelector('.winecards__item') as HTMLElement)!.style.pointerEvents = 'none';
-        (wineCard.querySelector('.winecards__item') as HTMLElement)!.style.opacity = '0.5';
-        (wineCard.querySelector('.winecards__face-count'))!.textContent = 'Нет в наличии';
-        (wineCard.querySelector('.winecards__face-price'))!.textContent = '';
+        wineCard.node.style.pointerEvents = 'none';
+        wineCard.node.style.opacity = '0.5';
+        wineCard.node.textContent = 'Нет в наличии';
+        wineCard.node.textContent = '';
       }
 
       if(item.inCart > 0) {
-        (wineCard.querySelector('.winecards__item') as HTMLElement)!.classList.add('inCart');
+        wineCard.node.classList.add('inCart');
       }
       else{
-        (wineCard.querySelector('.winecards__item') as HTMLElement)!.classList.remove('inCart');
+        wineCard.node.classList.remove('inCart');
 
       }
-      (wineCard.querySelector('.cart__add'))!.setAttribute('data-articul', `${idx}`)
-      fragment.append(wineCard);
+      wineCard.node.setAttribute('data-articul', `${idx}`)
     });
+  }
 
-    (document.querySelector('.winecards-container'))!.innerHTML = '';
-    (document.querySelector('.winecards-container'))!.appendChild(fragment);
+  rerender() {
+    this.wineContainer.destroy();
+    this.render();
   }
 }
 
--- online-store/online-store/src/utils/component.ts
+++ online-store/online-store/src/utils/component.ts
@@ -1,13 +1,30 @@
-export default class Component {
-  protected element: HTMLElement;
+// export default class Component {
+//   protected element: HTMLElement;
 
-  constructor(tagName: string, className: string = '', id?: string) {
-    this.element = document.createElement(tagName);
-    this.element.className = className;
-    id ? (this.element.id = id) : null;
+//   constructor(tagName: string, className: string = '', id?: string) {
+//     this.element = document.createElement(tagName);
+//     this.element.className = className;
+//     id ? (this.element.id = id) : null;
+//   }
+
+//   render() {
+//     return this.element;
+//   }
+// }
+
+export default class Component {
+  node: HTMLElement;
+  constructor(parentNode: HTMLElement, tagName = '', className = '', content = '') {
+    const element = document.createElement(tagName);
+    element.className = className;
+    element.textContent = content;
+    if (parentNode) {
+      parentNode.append(element);
+    }
+    this.node = element;
   }
 
-  render() {
-    return this.element;
+  destroy() {
+    this.node.remove();
   }
 }
